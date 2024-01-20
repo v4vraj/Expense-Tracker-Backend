fcfdf928adb756dd1bc4c1898071fb3d9d22a1e8
vraj1763@gmail.com <vraj1763@gmail.com>
2024-01-20 12:43:52 +0530

    jwt and password crypt-dcrypt

---
+++ model/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
