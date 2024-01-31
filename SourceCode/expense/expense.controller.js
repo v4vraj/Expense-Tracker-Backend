f9e9c34b494864b9ee88bd791cb69e477d830288
vraj1763@gmail.com <vraj1763@gmail.com>
2024-01-31 20:17:29 +0530

    Updating expense Module

--- SourceCode/expense/expense.controller.js
+++ SourceCode/expense/expense.controller.js
@@ -2,14 +2,12 @@ const Expense = require("./model/expense.model");
 
 const addExpenses = async (req, res) => {
   try {
-    const { userId, description, amount, status } = req.body;
-    console.log(userId, description, amount, status);
+    const { userId, description, amount } = req.body;
 
     const newExpense = new Expense({
       userId,
       description,
       amount,
-      status,
     });
 
     await newExpense.save();
@@ -24,26 +22,8 @@ const addExpenses = async (req, res) => {
 const getExpenses = async (req, res) => {
   try {
     const userId = req.query.userId;
-    const page = parseInt(req.query.page) || 1;
-    const itemsPerPage = parseInt(req.query.itemsPerPage) || 3;
-    const sortOrder = req.query.sortOrder === "desc" ? -1 : 1;
-    const filterStatus = req.query.filterStatus || ""; // Change here
-    const skip = (page - 1) * itemsPerPage;
-    console.log(filterStatus);
-    // Define the filter based on filterStatus
-    const filter = filterStatus
-      ? { userId, status: filterStatus } // Change here
-      : { userId };
-
-    const totalExpenses = await Expense.countDocuments(filter);
-    const totalPages = Math.ceil(totalExpenses / itemsPerPage);
-
-    const expenses = await Expense.find(filter)
-      .skip(skip)
-      .limit(itemsPerPage)
-      .sort({ description: sortOrder });
-
-    res.status(200).json({ expenses, totalPages });
+    const expenses = await Expense.find({ userId });
+    res.status(200).json(expenses);
   } catch (error) {
     console.error("Error fetching Expenses", error);
     res.status(500).json({ error: "Internal Server Error" });
